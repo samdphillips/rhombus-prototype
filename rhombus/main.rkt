22125763bc2fd0b4eb767786a8737acaf435c8d9
Sam Phillips <samdphillips@gmail.com>
2021-11-15 16:17:35 -0800

    Add `new` syntax for creating class instances

--- rhombus/main.rkt
+++ rhombus/main.rkt
@@ -35,7 +35,8 @@
         "private/quasiquote.rkt"
         "private/keyword.rkt"
         "private/symbol.rkt"
-        "private/values.rkt")
+        "private/values.rkt"
+        "private/racket-class.rkt")
 
 (module reader syntax/module-reader
   #:language 'rhombus
--- rhombus/private/racket-class.rkt
+++ rhombus/private/racket-class.rkt
@@ -1,8 +1,13 @@
 #lang racket/base
 
-(require racket/class)
+(require (for-syntax racket/base
+                     syntax/parse)
+         rhombus/private/expression
+         rhombus/private/parse
+         racket/class)
 
-(provide object?
+(provide (rename-out [rhombus-new new])
+         object?
          object-dot-lookup)
 
 (define (object-has-field? o field)
@@ -25,3 +30,27 @@
 
     [else (fail)]))
 
+(begin-for-syntax
+  (define-syntax-class :init-field
+    #:datum-literals (block group)
+    #:attributes (name expr)
+    [pattern (group name-kw:keyword (block (~and expr (group e ...))))
+             #:attr name
+             (datum->syntax
+              #'name-kw
+              (string->symbol
+               (keyword->string
+                (syntax->datum #'name-kw)))
+              #'name-kw)]))
+
+(define-syntax rhombus-new
+  (expression-transformer
+   #'rhombus-new
+   (lambda (stx)
+     (syntax-parse stx
+       #:datum-literals (block group parens)
+       [(form-id cls-expr ... (parens init-field::init-field ...) . tail)
+        (values #'(new (rhombus-expression (group cls-expr ...))
+                       [init-field.name (rhombus-expression init-field.expr)]
+                       ...)
+                #'tail)]))))
